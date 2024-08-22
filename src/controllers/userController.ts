7909b44f606a7250c8676b67b0e125209ddb6241
lucas Emanoel <lucastheldl@gmail.com>
2024-08-22 09:28:57 -0300

    feat: imroved error handling in user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { LoginService } from "../services/loginService";
 import { CreateUserService } from "../services/createUserService";
 import { UserModel } from "../models/userModel";
+import { InvalidCredentialsError } from "../services/errors/invalid-credentials-error";
 
 export const createUser = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
@@ -11,10 +12,11 @@ export const createUser = async (req: Request, res: Response) => {
     const user = await createUserService.execute({ username, email, password });
 
     return res.status(200).json(user);
-  } catch (error: any) {
-    res.status(500).json({ message: "Algo deu errado" });
-    console.log(error.message);
-    throw new Error("Error trying to create user");
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      // 400: bad request
+      return res.status(400).send({ message: error.message });
+    }
   }
 };
 export const loginUser = async (req: Request, res: Response) => {
@@ -25,9 +27,10 @@ export const loginUser = async (req: Request, res: Response) => {
     const user = await userService.execute({ email, password });
 
     return res.status(200).json(user);
-  } catch (error: any) {
-    console.log(error.message);
-    res.status(500).json({ message: "Algo deu errado" });
-    throw new Error("Error trying to login user");
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      // 400: bad request
+      return res.status(400).send({ message: error.message });
+    }
   }
 };
